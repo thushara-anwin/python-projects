0b7b3490f07baba232a3bdbec5c381ab71c1db8c
thushara-anwin <thusharaanwin@gmail.com>
2024-05-26 12:21:20 -0500

    adding pytest project

--- _PytestFrameworkProjects/magento_pytest/conftest.py
+++ _PytestFrameworkProjects/magento_pytest/conftest.py
@@ -1,10 +1,31 @@
+import os
+import sys
+import pytest
+current_path = os.path.dirname(os.path.realpath(__file__))
+utilities_path = os.path.join(current_path, '..', 'utilities')
+sys.path.append(utilities_path)
 from selenium import webdriver
+import ReadConfigurations as readConfigurations
+# from utilities import ReadConfigurations
 
 
-def set_up_and_tear_down():
+
+@pytest.fixture()
+def set_up_and_tear_down(request):
+    browser = readConfigurations.read_configurations("basic info", "browser")
+    if browser.__eq__("chrome"):
+        driver = webdriver.Chrome()
+    elif browser.__eq__("firefox"):
+        driver = webdriver.Firefox()
+    elif browser.__eq__("edge"):
+        driver = webdriver.Edge()
+    else:
+        print("Provide a valid name")
     driver = webdriver.Chrome()
     driver.maximize_window()
     driver.implicitly_wait(10)
-    driver.get("https://magento.softwaretestingboard.com/")
+    URL = readConfigurations.read_configurations("basic info", "url")
+    driver.get(URL)
+    request.cls.driver = driver
     yield
     driver.close()
--- _PytestFrameworkProjects/magento_pytest/test_login.py
+++ _PytestFrameworkProjects/magento_pytest/test_login.py
@@ -1,60 +1,43 @@
 import random
 import pytest
-from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 
-@pytest.fixture()
-def set_up_and_tear_down():
-    global driver
-    driver=webdriver.Chrome()
-    driver.get("https://magento.softwaretestingboard.com/")
-    driver.maximize_window()
-    driver.implicitly_wait(10)
-    yield
-    driver.close()
+@pytest.mark.usefixtures("set_up_and_tear_down")
+class TestLogin:
+    def test_login_with_valid_credential(self):
+        self.driver.find_element(By.XPATH,
+                            "//ul[@class='header links']"
+                            "//following::li[@data-label='or']/a[contains(text(),'Sign In')][1]").click()
+        self.driver.find_element(By.CSS_SELECTOR, "input#email").send_keys('thushara@gmail.com')
+        self.driver.find_element(By.CSS_SELECTOR, "input[name='login[password]']").send_keys('south_middle_123')
+        self.driver.find_element(By.XPATH,"//div[@class='primary']"
+                                     "/button[@id='send2' and @type ='submit' and @name ='send']/span").click()
+        displayed_message = 'Welcome'
+        assert self.driver.find_element(By.XPATH,"//span[contains(text(),'Welcome')]").text.__contains__(displayed_message)
+    def test_login_with_invalid_email_valid_password(self):
+        self.driver.find_element(By.CSS_SELECTOR,"li.authorization-link>a").click()
+        self.driver.find_element(By.CSS_SELECTOR, "input#email").send_keys(self.random_email())
+        self.driver.find_element(By.CSS_SELECTOR, "input[name='login[password]']").send_keys('south_middle_123')
+        self.driver.find_element(By.CSS_SELECTOR,"div+div>div.primary>button#send2").click()
+        assert self.driver.find_element(By.CSS_SELECTOR,"div[data-ui-id='message-error']").is_displayed()
+    def test_login_with_valid_email_invalid_password(self):
+        self.driver.find_element(By.CSS_SELECTOR,"li.authorization-link>a").click()
+        self.driver.find_element(By.CSS_SELECTOR, "input#email").send_keys('thushara@gmail.com')
+        self.driver.find_element(By.CSS_SELECTOR, "input[name='login[password]']").send_keys('123')
+        self.driver.find_element(By.CSS_SELECTOR,"div+div>div.primary>button#send2").click()
+        assert self.driver.find_element(By.CSS_SELECTOR,"div[data-ui-id='message-error']").is_displayed()
+    def test_login_with_no_credentials(self):
+        self.driver.find_element(By.CSS_SELECTOR,"li.authorization-link>a").click()
+        self.driver.find_element(By.CSS_SELECTOR, "input#email").send_keys('')
+        self.driver.find_element(By.CSS_SELECTOR, "input[name='login[password]']").send_keys('')
+        expected_warning_message='This is a required field.'
+        self.driver.find_element(By.CSS_SELECTOR,"div+div>div.primary>button#send2").click()
+        assert self.driver.find_element(By.CSS_SELECTOR,"div#email-error").text.__contains__(expected_warning_message)
+        assert self.driver.find_element(By.CSS_SELECTOR,"div#pass-error").is_displayed()
 
-
-def test_login_with_valid_credential(set_up_and_tear_down):
-    driver.find_element(By.XPATH,
-                        "//ul[@class='header links']"
-                        "//following::li[@data-label='or']/a[contains(text(),'Sign In')][1]").click()
-    driver.find_element(By.CSS_SELECTOR, "input#email").send_keys('thushara@gmail.com')
-    driver.find_element(By.CSS_SELECTOR, "input[name='login[password]']").send_keys('south_middle_123')
-    driver.find_element(By.XPATH,"//div[@class='primary']"
-                                 "/button[@id='send2' and @type ='submit' and @name ='send']/span").click()
-    displayed_message = 'Welcome'
-    assert driver.find_element(By.XPATH,"//span[contains(text(),'Welcome')]").text.__contains__(displayed_message)
-
-
-def test_login_with_invalid_email_valid_password(set_up_and_tear_down):
-    driver.find_element(By.CSS_SELECTOR,"li.authorization-link>a").click()
-    driver.find_element(By.CSS_SELECTOR, "input#email").send_keys(random_email())
-    driver.find_element(By.CSS_SELECTOR, "input[name='login[password]']").send_keys('south_middle_123')
-    driver.find_element(By.CSS_SELECTOR,"div+div>div.primary>button#send2").click()
-    assert driver.find_element(By.CSS_SELECTOR,"div[data-ui-id='message-error']").is_displayed()
-
-
-def test_login_with_valid_email_invalid_password(set_up_and_tear_down):
-    driver.find_element(By.CSS_SELECTOR,"li.authorization-link>a").click()
-    driver.find_element(By.CSS_SELECTOR, "input#email").send_keys('thushara@gmail.com')
-    driver.find_element(By.CSS_SELECTOR, "input[name='login[password]']").send_keys('123')
-    driver.find_element(By.CSS_SELECTOR,"div+div>div.primary>button#send2").click()
-    assert driver.find_element(By.CSS_SELECTOR,"div[data-ui-id='message-error']").is_displayed()
-
-
-def test_login_with_no_credentials(set_up_and_tear_down):
-    driver.find_element(By.CSS_SELECTOR,"li.authorization-link>a").click()
-    driver.find_element(By.CSS_SELECTOR, "input#email").send_keys('')
-    driver.find_element(By.CSS_SELECTOR, "input[name='login[password]']").send_keys('')
-    expected_warning_message='This is a required field.'
-    driver.find_element(By.CSS_SELECTOR,"div+div>div.primary>button#send2").click()
-    assert driver.find_element(By.CSS_SELECTOR,"div#email-error").text.__contains__(expected_warning_message)
-    assert driver.find_element(By.CSS_SELECTOR,"div#pass-error").is_displayed()
-
-
-def random_email():
-    num = random.randint(10,90)
-    num =str(num)
-    return 'thushara'+ num +'@gmail.com'
+    def random_email(self):
+        num = random.randint(10,90)
+        num =str(num)
+        return 'thushara'+ num +'@gmail.com'
 
